ab893e79fac0cba127fc868169b5e2206f12809c
Gunasekaraselvapandian <gssp02826@gmail.com>
2026-06-17 17:14:49 +0530

    feat: add route for uploading resume using multer

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const multer = require('multer');
 
 const app = express();
 
@@ -10,8 +11,19 @@ app.use(cors());
 
 dotenv.config();
 
+const upload = multer({ dest: 'uploads/' });
+
 app.get('/api/test', (req, res) => {
   res.send('The server is working.');
+});
+
+app.post('/api/analyze', upload.single('resume'), (req, res) => {
+  if (!req.file) {
+    return res.status(400).send('No file uploaded.');
+  }
+
+  console.log("Uploaded file:", req.file);
+  res.send("File Uploaded Successfully!");
 });
 
 app.listen(3000, () => console.log('Server is running on port 3000'));
